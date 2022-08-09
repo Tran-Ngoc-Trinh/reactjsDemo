de73329cccfe1fdcd429ba6069481d423c2bda06
Trinh <trinhtran@bravesoft.com.vn>
2022-08-09 15:58:50 +0700

    m

---
+++ src/components/Layout/DefaultLayout/Footer/index.js
@@ -0,0 +1,32 @@
+import classNames from 'classnames/bind';
+import styles from './Footer.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faArrowRightToBracket } from '@fortawesome/free-solid-svg-icons';
+
+const cx = classNames.bind(styles);
+
+function Footer() {
+    return (
+        <footer className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <div className={cx('content1')}>
+                    <p>LOCATION</p>
+                    <span>103 Nguyen Thong Quan 3 HCM</span>
+                </div>
+                <div className={cx('content2')}>
+                    <p>AROUND THE WEB</p>
+                    <FontAwesomeIcon icon={faCircleXmark}></FontAwesomeIcon>
+                    <FontAwesomeIcon icon={faCircleXmark}></FontAwesomeIcon>
+                    <FontAwesomeIcon icon={faCircleXmark}></FontAwesomeIcon>
+                    <FontAwesomeIcon icon={faCircleXmark}></FontAwesomeIcon>
+                </div>
+                <div className={cx('content3')}>
+                    <p>ABOUT FREELANCER</p>
+                    <span>Freelancer is a free to use, MIT licensed Bootstrap theme created by Start Bootstrap.</span>
+                </div>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/components/Layout/DefaultLayout/Header/index.js
@@ -0,0 +1,38 @@
+import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faArrowRightToBracket } from '@fortawesome/free-solid-svg-icons';
+import styles from './Header.module.scss';
+import logo from './../../../../assets/images/logo.jpg';
+
+const cx = classNames.bind(styles);
+
+function Header() {
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={logo} alt="logo" />
+                    <span>TheThem</span>
+                </div>
+                <div className={cx('search')}>
+                    <input placeholder="Search theme" spellCheck={false}></input>
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark}></FontAwesomeIcon>
+                    </button>
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('actions')}>
+                    <a href="#">About</a>
+                    <a href="#">Contact</a>
+                    <a href="#">Theme</a>
+                    <FontAwesomeIcon icon={faArrowRightToBracket} />
+                    <a className={cx('login')}>Login</a>
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
